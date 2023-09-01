8131ac7a505786e8b633b95bd1ce4d61d2821b36
saulo <saulodavi2001@gmail.com>
2023-09-01 20:54:38 -0300

    Operations Exercices 3/3

---
+++ NumericOperations.js/Average.js
@@ -0,0 +1,13 @@
+// Crie uma função chamada diferencaEntreMedias(n1, n2) que receba dois números e calcule dois valores diferentes, que chamaremos de media1 e media2. Eles devem ser calculados seguindo as expressões:
+
+// media1 = (n1+n2)/2
+// media2 = n1+n2/2
+// Faça com que a função retorne a diferença entre essas médias (media2 - media1)
+
+// Você sabe dizer porque os valores dessas variáveis são diferentes?
+
+function diferencaEntreMedias(n1,n2){
+    let media1 = (n1+n2)/2
+    let media2 = n1+n2/2
+    return(media2-media1)
+}
