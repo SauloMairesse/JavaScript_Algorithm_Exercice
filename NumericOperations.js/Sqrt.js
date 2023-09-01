175b7913219b2dad0ad529be8f9e33537c87d3fd
saulo <saulodavi2001@gmail.com>
2023-09-01 20:52:06 -0300

    Operations Exercices 1/3

---
+++ NumericOperations.js/Sqrt.js
@@ -0,0 +1,6 @@
+// Complete a função chamada retornaRaiz() que deve receber um número como parâmetro e retonar a raiz quadrada do valor recebido.
+
+//Altere a função para que ela faça o que foi pedido
+function retornaRaiz(numero) {
+    return(Math.sqrt(numero))
+}
