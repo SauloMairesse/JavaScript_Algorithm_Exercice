31405073119d3e875e5450a11bdc19ef6db85b3c
saulo <saulodavi2001@gmail.com>
2023-09-01 20:53:17 -0300

    Operations Exercices 2/3

---
+++ NumericOperations.js/DownArround.js
@@ -0,0 +1,6 @@
+
+// Pesquise sobre o método floor(x) da biblioteca Math do JavaScript e crie uma função chamada arredondaParaBaixo(x), que recebe como parâmetro um número decimal e retorna o maior inteiro menor ou igual ao número recebido.
+
+function arredondaParaBaixo(x) {
+    return(Math.floor(x))
+}
---
+++ NumericOperations.js/arround.js
@@ -0,0 +1,6 @@
+
+// Utilize o método round(x) da biblioteca Math do JavaScript para criar uma função chamada arredonda(x), que recebe como parâmetro um número decimal, e retorna o inteiro mais próximo.
+
+function arredonda(x) {
+    return(Math.round(x))
+}
