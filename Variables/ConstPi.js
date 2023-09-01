6ee9405a49c81e83a6c6923d26f1e429d62cb4ec
saulo <saulodavi2001@gmail.com>
2023-09-01 20:49:41 -0300

    Variables Exercices

---
+++ Variables/ConstPi.js
@@ -0,0 +1,7 @@
+// Crie uma função chamada calculaArea(r) que recebe um parâmetro numérico e retorna a área de um círculo com esse valor de raio.
+// Crie na sua função uma constante chamada pi que armazene o valor 3.1415 para utilizar na conta.
+
+function calculaArea(r){
+    const PI = 3.1415
+    return(PI*r**2)
+}
---
+++ Variables/SpeedMath.js
@@ -0,0 +1,8 @@
+// Crie uma função chamada calculaVelocidade(v0, a, t) que recebe três parâmetros, que representam respectivamente a velocidade inicial, a aceleração e o instante de tempo do movimento de uma partícula e retorna a velocidade dessa partícula, nesse instante t, a partir da fórmula da função horária do movimento:
+// V = v0 + a*t
+// Crie uma variável chama velocidade que receba o valor da expressão acima, e retorne essa variável.
+
+function calculaVelocidade(v0,a,t){
+    let velocidade = v0+a*t
+    return(velocidade)
+} 
